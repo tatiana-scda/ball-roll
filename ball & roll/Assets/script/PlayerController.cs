4afd89af21907fd1e718f264fcbe4ec440cccc87
tatiana <tatiana-scda@gmail.com>
2019-01-13 21:36:38 -0200

    game playable

---
+++ ball & roll/Assets/script/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+    public Text pointsText;
+    public Text winText;
+
+    private Rigidbody rb;
+    private int points;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        points = 0;
+        setPointuationText();
+        winText.text = ""; 
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("pick up"))
+        {
+            other.gameObject.SetActive(false);
+            points++;
+            setPointuationText();
+        }
+    }
+
+    void setPointuationText()
+    {
+        pointsText.text = "Points: " + points.ToString();
+        if (points >= 8)
+        {
+            winText.text = "You win!";
+        }
+    }
+}
